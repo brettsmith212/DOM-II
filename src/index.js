9329813e5b1a2a78665a7c70f5bd9a132ddcfcde
Brett Smith <brettsmith212@gmail.com>
2021-11-30 12:45:58 -0800

    6 total addEventListeners

--- src/index.js
+++ src/index.js
@@ -4,12 +4,42 @@ import "./less/index.less";
 
 const logo = document.querySelector("h1.logo-heading");
 const homeImg = document.querySelector(".intro img");
+const h2Intro = document.querySelector(".intro h2");
+const body = document.querySelector("body");
+const navbarAnchors = document.querySelectorAll("a");
+
+function darkMode(e) {
+  if (e.key === "d") {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+    logo.style.color = "black";
+  }
+}
+function lightMode(e) {
+  if (e.key === "l") {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  }
+}
 
 logo.addEventListener("mouseover", (e) => {
   e.target.style.color = "red";
 });
 
-homeImg.addEventListener("dblclick", (e) => {
+homeImg.addEventListener("mouseleave", (e) => {
   e.target.style.borderRadius = "10px";
   e.target.style.transform = "scale(1.1)";
+});
+
+h2Intro.addEventListener("click", (e) => {
+  e.target.style.transform = "scale(1.1)";
+});
+
+document.addEventListener("keydown", darkMode);
+document.addEventListener("keyup", lightMode);
+
+navbarAnchors.forEach((anchor) => {
+  anchor.addEventListener("dblclick", (e) => {
+    e.target.style.color = "#339af0";
+  });
 });
