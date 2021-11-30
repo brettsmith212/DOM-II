84e26f57b77e2ff549241c2cd3c7a933fdcfbb79
Brett Smith <brettsmith212@gmail.com>
2021-11-30 13:13:16 -0800

    10 Event Listeners and 1 preventDefault()

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const homeImg = document.querySelector(".intro img");
 const h2Intro = document.querySelector(".intro h2");
 const body = document.querySelector("body");
 const navbarAnchors = document.querySelectorAll("a");
+const adventureImg = document.querySelector(".img-content img:nth-of-type(1)");
+const h2All = document.querySelectorAll("h2");
+const buttons = document.querySelectorAll(".btn");
+console.log(buttons);
 
 function darkMode(e) {
   if (e.key === "d") {
@@ -31,7 +35,7 @@ homeImg.addEventListener("mouseleave", (e) => {
   e.target.style.transform = "scale(1.1)";
 });
 
-h2Intro.addEventListener("click", (e) => {
+h2Intro.addEventListener("dblclick", (e) => {
   e.target.style.transform = "scale(1.1)";
 });
 
@@ -39,7 +43,32 @@ document.addEventListener("keydown", darkMode);
 document.addEventListener("keyup", lightMode);
 
 navbarAnchors.forEach((anchor) => {
-  anchor.addEventListener("dblclick", (e) => {
+  anchor.addEventListener("click", (e) => {
+    e.preventDefault();
     e.target.style.color = "#339af0";
+  });
+});
+
+adventureImg.addEventListener("mousewheel", (e) => {
+  e.target.style.transform = "rotate(180deg)";
+});
+
+adventureImg.addEventListener("mousedown", (e) => {
+  e.target.style.transform = "rotate(360deg)";
+});
+
+h2All.forEach((h) => {
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "m") {
+      console.log(e.key);
+      h.style.transform = "translate(50px)";
+      h.style.color = "pink";
+    }
+  });
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("mousemove", (e) => {
+    e.target.style.transform = "rotate(180deg)";
   });
 });
